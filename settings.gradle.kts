9a27bc1911cb75b4a4817d23fb2b8a413de88d33
HatePK <hatepk@gmail.com>
2024-02-22 22:18:14 +0300

    start project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Resp-toi App"
+include(":app")
